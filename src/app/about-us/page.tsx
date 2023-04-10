84b7df335cdca1fe62f8b278df8d8ee43b23c8e7
Touha Zohair <touha98@gmail.com>
2023-04-10 14:40:39 +0600

    about us page

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,5 +1,58 @@
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return (
+    <main className="container lg:max-w-screen-lg p-2 mx-auto">
+      <article className="prose prose-slate dark:prose-invert md:prose-lg mt-6">
+        <h2>Welcome to Gadget Era</h2>
+        <p>
+          Gadget Era is an online wristwatch store based in Bangladesh, offering
+          a wide variety of high-quality watches for men and women. We are
+          passionate about providing our customers with a convenient and
+          enjoyable shopping experience, and we pride ourselves on our
+          commitment to quality, affordability, and customer service.
+        </p>
+
+        <h2>Our Mission and Values</h2>
+        <p>
+          Our mission at Gadget Era is to offer stylish and functional watches
+          that meet the needs and preferences of our customers. We are dedicated
+          to providing watches that are durable, reliable, and affordable, and
+          we strive to ensure that every customer is satisfied with their
+          purchase. Our core values include:
+        </p>
+        <ul>
+          <li>Integrity</li>
+          <li>Customer satisfaction</li>
+          <li>Quality</li>
+          <li>Affordability</li>
+        </ul>
+
+        <h2>Our Products</h2>
+        <p>
+          At Gadget Era, we offer a wide range of wristwatches for men and
+          women, including popular brands such as Naviforce, Curren, OLEVS and
+          more. We source our watches from trusted manufacturers to ensure that
+          every product meets our high standards for quality and style.
+        </p>
+
+        <h2>Our Team</h2>
+        <p>
+          Our team at Gadget Era is made up of experienced and knowledgeable
+          professionals who are dedicated to providing exceptional customer
+          service. From our customer support staff to our logistics team, every
+          member of our team is committed to ensuring that our customers have a
+          positive shopping experience with us.
+        </p>
+
+        <h2>Contact Us</h2>
+        <p>
+          If you have any questions or comments about our products or services,
+          please don&apos;t hesitate to contact us. You can reach us by phone at
+          +880 1517-019546, by email at info@gadgeterabd.com, or through our
+          facebook page.
+        </p>
+      </article>
+    </main>
+  );
 };
 
 export default AboutUs;
