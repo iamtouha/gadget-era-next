7bd8ecf8e6c7385cccf85be0ea73f56f655a09f3
Touha Zohair <touha98@gmail.com>
2023-08-10 14:11:03 +0600

    img generator fn

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,1 +1,16 @@
-import { ImageResponse } from "next/server";
+import generateImage from "@/lib/functions/generateOg";
+
+export const runtime = "edge";
+
+export const alt = "Gadget Era";
+
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+export default function Image() {
+  return generateImage("Welcome to Gadget Era");
+}
